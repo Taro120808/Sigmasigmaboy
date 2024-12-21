67d37196ddc18dc561d88921710c6e47749c18d2
Shiro-Yurei <pjasonsarceno@gmail.com>
2024-12-21 10:19:29 +0800

    Update-1.1(fixed) Main.py

--- Main.py
+++ Main.py
@@ -3,7 +3,7 @@ from sys import exit
 
 pygame.init()
 screen = pygame.display.set_mode((800, 400))
-pygame.display.set_caption('runner')
+pygame.display.set_caption('Aswang')
 clock = pygame.time.Clock()
 
 while True:
