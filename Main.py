cde6e99cc7edc6adcf9f577d5e3fe0536e450211
Shiro-Yurei <pjasonsarceno@gmail.com>
2025-01-10 23:37:52 +0800

    Update Main-1.2(fixed).py

--- Main.py
+++ Main.py
@@ -4,18 +4,18 @@ from sprites import *
 import sys
 
 class Game:
-    def __init__(self):
-        pygame.init()
-        self.screen = pygame.display.set_mode((WIN_WIDTH, WIN_HEIGHT))
-        self.clock = pygame.time.Clock()
-        self.font = pygame.font.Font('League Spartan', 32)
-        self.running = True
+        def __init__(self):
+                pygame.init()
+                self.screen = pygame.display.set_mode((WIN_WIDTH, WIN_HEIGHT))
+                self.clock = pygame.time.Clock()
+                self.font = pygame.font.Font('League Spartan', 32)
+                self.running = True
 
-    def new(self):
-        self.playing = True
+        def new(self):
+                self.playing = True
 
-        self.all_sprites = pygame.sprite.LayeredUpdates()
-        self.blocks = pygame.sprite.LayeredUpdates()
-        self.mob1 = pygame.sprite.LayeredUpdates()
+                self.all_sprites = pygame.sprite.LayeredUpdates()
+                self.blocks = pygame.sprite.LayeredUpdates()
+                self.mob1 = pygame.sprite.LayeredUpdates()
 
-        self.player = Player()
+                self.player = Player(self, 1, 2)
