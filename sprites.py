ed89e91c27f3496d587c707457591f14a4f3dcda
Taro <shawnraycamilan@gmail.com>
2025-01-16 02:45:04 +0800

    sprites.py

--- sprites.py
+++ sprites.py
@@ -7,16 +7,16 @@ class Player(pygame.sprite.Sprite):
         def __init__(self, game, x, y):
 
                 self.game = game
-                player._layer =  PLAYER_LAYER
+                self._layer =  PLAYER_LAYER
                 self.groups = self.game.all_sprites
-                pygame.sprite.SPRITE.__init__(self, self.groups)
+                pygame.sprite.Sprite.__init__(self, self.groups)
 
                 self.x = x * TILESIZE
                 self.y = y * TILESIZE
                 self.HEIGHT = TILESIZE
                 self.WIDTH = TILESIZE
 
-                self.image = pygame.Surface([self.width, self.height])
+                self.image = pygame.Surface([self.WIDTH, self.HEIGHT])
                 self.image.fill(RED) #will be changed in a future update
 
                 self.rect = self.image.get_rect()
